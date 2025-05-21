a34abbba9f6af468eb3daff7750ee1babd698454
Abhirama Sonny <abhirama.sonny@gmail.com>
2025-05-21 17:33:40 -0500

    push nonbreaking code for once shyam 🙏

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ RobotHardware    robot(master);
 void initialize() {
   pros::lcd::initialize();
   pros::lcd::set_text(1, "Press A/B/X to select auton");
-  pros::lcd::set_text(2, "Press UP/DOWN to select color")
+  pros::lcd::set_text(2, "Press UP/DOWN to select color");
   robot.init();
 }
 
@@ -23,26 +23,26 @@ void competition_initialize() {
     if (master.get_digital_new_press(DIGITAL_A)) {
       selectedAuton = AutonMode::SKILLS;
       pros::lcd::set_text(4, "Auton: Skills");
-      master.set_text(0, 0, "Auton: Skills")
+      master.set_text(0, 0, "Auton: Skills");
     }
     if (master.get_digital_new_press(DIGITAL_B)) {
       selectedAuton = AutonMode::LEFT_GOAL;
       pros::lcd::set_text(4, "Auton: Left");
-      master.set_text(0, 0, "Auton: Left")
+      master.set_text(0, 0, "Auton: Left");
     }
     if (master.get_digital_new_press(DIGITAL_X)) {
       selectedAuton = AutonMode::RIGHT_GOAL;
       pros::lcd::set_text(4, "Auton: Right");
-      master.set_text(0, 0, "Auton: Right")
+      master.set_text(0, 0, "Auton: Right");
     }
 
     if(master.get_digital_new_press(DIGITAL_UP)) {
-      pros::lcd::set_background_color(245, 66, 66);
+      //pros::lcd::set_background_color(245, 66, 66);
       pros::lcd::set_text(5, "Red Alliance");
       master.set_text(1, 0, "Red Alliance");
     }
     if(master.get_digital_new_press(DIGITAL_DOWN)) {
-      pros::lcd::set_background_color(66, 135, 245);
+      //pros::lcd::set_background_color(66, 135, 245);
       pros::lcd::set_text(5, "Blue Alliance");
       master.set_text(1, 0, "Blue Alliance");
     }
