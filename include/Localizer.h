e36a51575485713d189d8c84d41dda23e87ebc99
Abhirama Sonny <abhirama.sonny@gmail.com>
2025-05-22 22:42:43 -0500

    localizer smh

--- include/Localizer.h
+++ include/Localizer.h
@@ -10,11 +10,10 @@
 #include "pros/distance.hpp"
 
 struct Particle {
-  double x, y, theta;   // pose in inches & degrees
+  double x, y, theta;
   double weight;
 };
 
-/// Particle‐filter localizer using diff‐drive odometry, IMU, & 4 range sensors
 class Localizer {
 public:
   Localizer(DriveBase             &drive,
@@ -24,19 +23,11 @@ public:
             pros::Distance        &backDist,
             pros::Distance        &leftDist);
 
-  /// Uniformly scatter particles across the field
   void initUniform();
-
-  /// 1) Propagate via motion model
   void motionUpdate();
-
-  /// 2) Weight via range‐measurement model
   void measurementUpdate();
-
-  /// 3) Resample particles
   void resample();
 
-  /// Get the current pose estimate (x, y, theta)
   std::tuple<double,double,double> getEstimate() const;
 
 private:
@@ -48,11 +39,10 @@ private:
   double prevLeftDeg  = 0.0;
   double prevRightDeg = 0.0;
   
-  // Helpers
   double inchesPerDegree() const {
     return RobotValues::WHEEL_DIAMETER_INCH * M_PI / 360.0;
   }
   double gaussian(double mu, double sigma, double x);
 };
 
-#endif // LOCALIZER_H
+#endif
